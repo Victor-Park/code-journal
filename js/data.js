f7181c842040343fc2466fc4fb64e506fb4eab52
Victor-Park <jsxpark@yahoo.com>
2022-04-05 19:59:18 +0000

    completed issue-1-create-an-entry

--- js/data.js
+++ js/data.js
@@ -1,8 +1,17 @@
 /* exported data */
-
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+var previousData = localStorage.getItem('local-storage');
+if (previousData !== null) {
+  data = JSON.parse(previousData);
+}
+
+window.addEventListener('beforeunload', function () {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('local-storage', dataJSON);
+});
--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 /* global data */
 /* exported data */
 
-var $urlInput = document.querySelector('#photo-url');
-var $img = document.querySelector('img');
+var url = document.querySelector('#photo-url');
+var img = document.querySelector('img');
 
-$urlInput.addEventListener('input', function updateImgSource(event) {
-  $img.src = $urlInput.value;
+url.addEventListener('input', function updateImgSource(event) {
+  img.src = url.value;
+});
+
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var userInput = {};
+  userInput.title = form.elements.title.value;
+  userInput.url = form.elements.url.value;
+  userInput.notes = form.elements.notes.value;
+  userInput.entryId = data.entries.length + 1;
+  ++data.nextEntryId;
+  data.entries.push(userInput);
+  img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  form.reset();
 });
